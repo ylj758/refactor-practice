caa239e02262c9b4a564bf2f1fce4d80269e19f8
ylj <758766951@qq.com>
2020-09-14 21:18:59 +0800

    [yulvjun]:implement 5

--- 5/src/main/java/com/twu/refactoring/Receipt.java
+++ 5/src/main/java/com/twu/refactoring/Receipt.java
@@ -1,15 +1,6 @@
 package com.twu.refactoring;
 
 public class Receipt {
-    private static final int FIXED_CHARGE = 50;
-    private static final double PEAK_TIME_MULTIPLIER = 1.2;
-    private static final double OFF_PEAK_MULTIPLIER = 1.0;
-    private static final int RATE_CHANGE_DISTANCE = 10;
-    private static final int PRE_RATE_CHANGE_NON_AC_RATE = 15;
-    private static final int POST_RATE_CHANGE_NON_AC_RATE = 12;
-    private static final int PRE_RATE_CHANGE_AC_RATE = 20;
-    private static final int POST_RATE_CHANGE_AC_RATE = 17;
-    private static final double SALES_TAX_RATE = 0.1;
 
     private final Taxi taxi;
 
@@ -19,21 +10,21 @@ public class Receipt {
 
     public double getTotalCost() {
         double totalCost = 0;
-
-        // fixed charges
-        totalCost += FIXED_CHARGE;
+        totalCost += StaticFinals.FIXED_CHARGE;
 
         // taxi charges
         int totalKms = taxi.getTotalKms();
-        double peakTimeMultiple = taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
+        double peakTimeMultiple = taxi.isPeakTime() ? StaticFinals.PEAK_TIME_MULTIPLIER : StaticFinals.OFF_PEAK_MULTIPLIER;
+        double mathMinCost= Math.min(StaticFinals.RATE_CHANGE_DISTANCE, totalKms) * peakTimeMultiple;
+        double mathMaxCost= Math.max(0, totalKms - StaticFinals.RATE_CHANGE_DISTANCE) * peakTimeMultiple;
         if(taxi.isAirConditioned()) {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_AC_RATE * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+            totalCost += mathMinCost * StaticFinals.PRE_RATE_CHANGE_AC_RATE ;
+            totalCost += mathMaxCost * StaticFinals.POST_RATE_CHANGE_AC_RATE ;
         } else {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+            totalCost += mathMinCost * StaticFinals.PRE_RATE_CHANGE_NON_AC_RATE ;
+            totalCost += mathMaxCost * StaticFinals.POST_RATE_CHANGE_NON_AC_RATE ;
         }
 
-        return totalCost * (1 + SALES_TAX_RATE);
+        return totalCost * (1 + StaticFinals.SALES_TAX_RATE);
     }
 }
---
+++ 5/src/main/java/com/twu/refactoring/StaticFinals.java
@@ -0,0 +1,13 @@
+package com.twu.refactoring;
+
+public class StaticFinals {
+    static final int FIXED_CHARGE = 50;
+    static final double PEAK_TIME_MULTIPLIER = 1.2;
+    static final double OFF_PEAK_MULTIPLIER = 1.0;
+    static final int RATE_CHANGE_DISTANCE = 10;
+    static final int PRE_RATE_CHANGE_NON_AC_RATE = 15;
+    static final int POST_RATE_CHANGE_NON_AC_RATE = 12;
+    static final int PRE_RATE_CHANGE_AC_RATE = 20;
+    static final int POST_RATE_CHANGE_AC_RATE = 17;
+    static final double SALES_TAX_RATE = 0.1;
+}
